4da9089195498ab8a662786c83d4f3fefc245ce0
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-16 04:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,9 +65,10 @@ for(var i = 0; i < cart.length; i++){
   if(cart[i].hasOwnProperty(item)) {
     cart.splice(i, 1);
     return cart;
-  } return 'That item is not in your cart.'
+  }
+} return 'That item is not in your cart.'
 }
-}
+
 
 function placeOrder(cardNumber) {
   // write your code here
