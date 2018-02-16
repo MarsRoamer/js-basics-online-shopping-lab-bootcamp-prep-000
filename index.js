2794bb5e90eb497dad5da9fa73dfad04ba06d427
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-16 04:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,10 +71,11 @@ for(var i = 0; i < cart.length; i++){
 
 
 function placeOrder(cardNumber) {
-  if(cardNumber === undefined) {
-    console.log("Sorry, we don't have a credit card on file for you")
-  } else {
+  if(cardNumber !== undefined) {
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
+  } else {
+  console.log("Sorry, we don't have a credit card on file for you")
+
   }
 }
