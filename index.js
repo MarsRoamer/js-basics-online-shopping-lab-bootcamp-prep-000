fb2de76bdedd0ecd069b132c143512f08af7d070
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-16 04:33:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ for(var i = 0; i < cart.length; i++){
 
 function placeOrder(cardNumber) {
   if(cardNumber !== null) {
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is , which will be charged to the card ${cardNumber}.`);
     cart = [];
   } else {
   console.log("Sorry, we don't have a credit card on file for you")
