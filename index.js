ca63ed225a461094401c4fff0c5824934d4c641d
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-16 04:32:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ for(var i = 0; i < cart.length; i++){
 
 
 function placeOrder(cardNumber) {
-  if(cardNumber !== 'undefined') {
+  if(cardNumber !== null) {
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   } else {
