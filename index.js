3e1bdb6b6e17fcf623f911e59c4d9c7653b096b3
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-16 04:15:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ for(var i = 0; i < cart.length; i++){
     cart.splice(i, 1);
     return cart;
   }
-} return 'That item is not in your cart.'
+} console.log('That item is not in your cart.');
 }
 
 
